b049e9209f2bd0b10c5a5c09cfba63ac84cec281
Rogério Leal <rogerionarcizoleal@gmail.com>
2026-05-18 11:11:41 -0300

    Subindo a tarefa do modulo 9

---
+++ tarefa_wrapper/src/wrapper.java
@@ -0,0 +1,10 @@
+public class wrapper {
+
+    public static void main(String[] args) {
+        int idade = 36;
+        System.out.println(idade);
+
+        Integer idade2 = idade;
+        System.out.println(idade2);
+    }
+}
